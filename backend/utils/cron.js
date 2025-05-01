dbb0991e58760c46384066977399f17af204ba45
Jdjohar <Jdeep514@gmail.com>
2025-05-01 15:29:02 +0530

    update 2

--- backend/utils/cron.js
+++ backend/utils/cron.js
@@ -8,7 +8,7 @@ function startCronJobs() {
   console.log("🟢 Starting cron jobs...");
 
   // Daily scans at 2:05 PM IST (14:05 UTC +5:30 = 8:35 UTC, but since you're using 'Asia/Kolkata', just use 14:05)
-  new CronJob('5 14 * * *', async () => {
+  new CronJob('35 15 * * *', async () => {
     console.log('🚀 Starting daily scan job at 2:05 PM IST...');
     try {
       const users = await User.find({});
