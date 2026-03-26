2617adce10c8673c866d39269edbd8d3f7b5d6f2
Jdjohar <Jdeep514@gmail.com>
2026-03-26 15:11:18 +0530

    new 42

--- backend/utils/cron.js
+++ backend/utils/cron.js
@@ -144,7 +144,7 @@ function startCronJobs() {
 
 
   // Daily scans at 03 15 PM IST
-  new CronJob('40 14 * * *', async () => {
+  new CronJob('20 15 * * *', async () => {
     console.log('🚀 Starting daily scan job at 10 18 PM IST...');
     try {
       const users = await User.find({});
